59876cccd8e95c875d8bedf679eb064fa19f40df
Imam Khaira <imamkhaira@gmail.com>
2020-01-31 20:17:03 +0800

    feat(navigation): reworked navigation
    rework on navigation layout
    
    BREAKING CHANGE: navigation only works on mobile now

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,113 +1,62 @@
 <template>
-  <div :class="`${$route.path !== '/' ? 'op-bg' : ''} py-12`">
-    <nav class="flex h-full items-center my-auto">
-      <img
-        src="@/static/logo.png"
-        srcset="@/static/logo@2x.png 2x, @/static/logo@3x.png 3x"
-        class="Logo ml-6 interactive"
-        alt="Grainer's logo"
-        @click="goHome"
-      />
-
-      <div class="ml-6">
-        <ul class="flex flex-row">
-          <svg class="inline absolute top-0" width="500" height="12">
-            <rect
-              x="0"
-              y="0"
-              :style="`${computedTranslate}; transition: all 0.3s ease-out;`"
-              width="51"
-              height="12"
-              fill="#014734"
-            />
-            <rect
-              x="0"
-              y="0"
-              :style="`${computedTranslate}; transition: all 0.5s ease-out;`"
-              width="51"
-              height="12"
-              fill="#02F6B6"
-            />
-          </svg>
-          <li class="interactive">
-            <nuxt-link to="/#home" prefetch class="mx-10 font-sh">Home</nuxt-link>
-          </li>
-          <li class="interactive">
-            <nuxt-link to="/about" prefetch class="mx-10 font-sh">About Us</nuxt-link>
-          </li>
-          <li class="interactive">
-            <nuxt-link to="/career" prefetch class="mx-10 font-sh">Career</nuxt-link>
-          </li>
-        </ul>
-      </div>
-      <button
-        class="absolute right-0 px-6 py-2 rounded-lg rounded-tr-none border-gr-green-light border-2 mr-12 font-sh outline-none slide interactive"
-        @click="goToContact"
-      >Contact Us</button>
-    </nav>
-  </div>
+  <nav :class="`navbar ${!isOpen && transparent ? 'transparent' : ''} ${addclass}`">
+    <img :src="require('@/static/logo@2x.png')" />
+    <button :class="`hamburger hamburger--minus ${isOpen ? 'is-active' : ''}`" type="button" @click="toggle">
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+  </nav>
 </template>
 
 <script>
 export default {
-  computed: {
-    computedTranslate() {
-      if (this.$route.path === '/') {
-        return `transform: translate(2.5em,0)`
-      } else if (this.$route.path === '/about') {
-        return `transform: translate(10em,0)`
-      } else if (this.$route.path === '/career') {
-        return `transform: translate(19em,0)`
-      }
-      return `transform: translate(2.5em,0)`
+  name: 'Navigation',
+  props: {
+    addclass: {
+      type: String,
+      default: ''
+    },
+    transparent: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {
+      isOpen: false
     }
   },
   methods: {
-    goHome() {
-      if (this.$route.path !== '/') {
-        const that = this
-        setTimeout(() => {
-          that.$router.push('/')
-          that.$store.commit('goto', '#home')
-        }, 500)
-      } else {
-        this.$store.commit('goto', '#home')
-      }
-    },
-    goToContact() {
-      if (this.$route.path !== '/') {
-        const that = this
-        setTimeout(() => {
-          that.$router.push('/')
-          that.$store.commit('goto', '#contact')
-        }, 500)
-      } else {
-        this.$store.commit('goto', '#contact')
-      }
+    toggle() {
+      this.isOpen = !this.isOpen
     }
   }
 }
 </script>
 
-<style>
-.logo {
-  width: 161.3px;
-  height: 72.6px;
-  object-fit: contain;
-}
+<style lang="scss" scoped>
+@import '../assets/css/hamburger.scss';
 
-/* And from the left */
-.slide:hover,
-.slide:focus {
-  box-shadow: inset 12em 0 0 0 #02f6b6;
-  color: #011b14;
-}
+.navbar {
+  position: fixed;
+  z-index: 100;
+  top: 0%;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
 
-.slide {
-  transition: 0.25s;
-}
+  &:not(.transparent) {
+    background-color: rgba($bg-color, 0.8);
+    backdrop-filter: blur(6px);
+  }
 
-.op-bg {
-  background-color: rgba(1, 27, 20, 0.877);
+  img {
+    width: 200px;
+  }
+
+  .transparent {
+    background-color: rgba(0, 0, 0, 0);
+  }
 }
 </style>
