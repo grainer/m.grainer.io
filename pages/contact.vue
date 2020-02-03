96bff7323288f763f178c3daa055733d37cee590
Imam Khaira <imamkhaira@gmail.com>
2020-02-03 17:30:15 +0800

    feat(contact): created contact page and component

---
+++ pages/contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="h-screen px-12">
+    <img :src="require('@/assets/img/mapbox.png')" class="mapbox" />
+    <contact></contact>
+  </div>
+</template>
+
+<script>
+import Contact from '@/components/Contact'
+
+export default {
+  components: {
+    contact: Contact
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.mapbox {
+  position: fixed;
+  bottom: 0%;
+  left: 0px;
+  width: 100vw;
+  z-index: -1;
+  opacity: 0.3;
+}
+</style>
